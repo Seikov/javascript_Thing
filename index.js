ecf8b2a1af3afe45b214d58cc40e4d0b3c0705a5
Seikov <154429340+Seikov@users.noreply.github.com>
2024-01-29 20:30:11 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
-var randomNumber1 = Math.round(Math.random() * 6);
-var randomNumber2 = Math.round(Math.random() * 6);
+var player1name = prompt("Player 1 name:")
+var player2name = prompt("Player 2 name:")
+document.getElementById("p1").innerHTML=player1name
+document.getElementById("p2").innerHTML=player2name
 
-if (randomNumber1 == 1) {
-document.getElementsByClassName("img1").setAttribute("src", "./images/dice1.png")
-} else if (randomNumber1 == 2) {
-    document.getElementsByClassName("img1").setAttribute("src", "./images/dice2.png")
-} else if (randomNumber1 == 3) {
-    document.getElementsByClassName("img1").setAttribute("src", "./images/dice3.png")
-} else if (randomNumber1 == 4) {
-    document.getElementsByClassName("img1").setAttribute("src", "./images/dice4.png")
-} else if (randomNumber1 == 5) {
-    document.getElementsByClassName("img1").setAttribute("src", "./images/dice5.png")
-} else if (randomNumber1 == 6) {
-    document.getElementsByClassName("img1").setAttribute("src", "./images/dice6.png")
+function diceGame(){
+    var dice1 = Math.floor((Math.random() * 6) + 1 );
+    var dice2 = Math.floor((Math.random() * 6) + 1);
+    
+    var dice1img = "./images/dice"+dice1+".png";
+    var dice2img = "./images/dice"+dice2+".png";
+ 
+    document.getElementsByClassName("img1")[0].setAttribute("src", dice1img);
+    document.getElementsByClassName("img2")[0].setAttribute("src", dice2img);
+ 
+    if (dice1 > dice2) {
+        document.getElementsByClassName("result")[0].innerHTML = player1name + " won!"
+    } else if (dice2 == dice1){
+        document.getElementsByClassName("result")[0].innerHTML = "Draw!"
+    } else {
+        document.getElementsByClassName("result")[0].innerHTML = player2name + " won!"
+    }
 }
